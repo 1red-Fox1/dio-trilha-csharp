fd77b8999868ef059f07df8f12ac57c0f0f1a112
RedFoX1029 <levinicolas2000@gmail.com>
2025-09-20 20:26:54 -0300

    [add] entrega do desafio POO de interfaces

---
+++ Desafio-Celulares/Desafio-Celulares/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+using Desafio_Celulares.Models;                                                                                                                
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111-1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "12222222-2", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
